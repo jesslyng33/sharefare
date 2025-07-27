617aa624199935e9df3f4917d12e826212878328
Jesslyn Gunadi <jesslyngunadi33@gmail.com>
2025-07-27 16:08:04 -0400

    basic set up for ride now stack navigator + home screen

--- client/navigation/TabNavigator.tsx
+++ client/navigation/TabNavigator.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/homeScreen';
 import FriendsScreen from '../screens/FriendsScreen';
-import RideNowScreen from '../screens/RideNowScreen';
 import ScheduleScreen from '../screens/ScheduleScreen';
 import YouStackNavigator from './YouStackNavigator';
+import RideNowStackNavigator from './RideNowStackNavigator';
 
 const Tab = createBottomTabNavigator();
 
@@ -18,7 +18,7 @@ export default function TabNavigator() {
     >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Friends" component={FriendsScreen} />
-      <Tab.Screen name="Ride Now" component={RideNowScreen} />
+      <Tab.Screen name="Ride Now" component={RideNowStackNavigator} />
       <Tab.Screen name="Schedule" component={ScheduleScreen} />
       <Tab.Screen name="You" component={YouStackNavigator} />
     </Tab.Navigator>
