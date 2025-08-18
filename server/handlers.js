e88576ca2cf4a953036c00c2559e5ee38643ba4d
Jesslyn Gunadi <jesslyngunadi33@gmail.com>
2025-08-18 15:10:02 -0400

    finish POST accept endpoint - check and respond to completed request

--- server/handlers.js
+++ server/handlers.js
@@ -4,17 +4,54 @@ export async function accept(req, res) {
   console.log('accept endpoint hit');
   const { id } = req.params;
 
-  const { data, error } = await supabase
+  const { data: request, error: errorOne } = await supabase
     .from('ride_now_requests')
     .update({
       status: 'accepted',
     })
-    .match({ id: id });
+    .match({ id: id })
+    .select('group_id')
+    .single()
 
-  if (error) {
-    console.error('Error updating row:', error);
+  if (errorOne) {
+    console.error('Error updating row:', errorOne);
   } else {
-    console.log('Row updated');
+    console.log('Row updated', request);
+  }
+
+  const groupId = request.group_id;
+
+  const { data: requests, error: errorTwo } = await supabase
+    .from('ride_now_requests')
+    .select('id, user_id, status, group_id')
+    .eq('group_id', groupId)
+
+  if (errorTwo) {
+    console.error('Error selecting requests:', errorTwo);
+  } else {
+    console.log('Requests selected');
+  }
+
+  let completed = true;
+  for (const request of requests) {
+    if (request.status !== 'accepted') {
+      completed = false;
+    }
+  }
+
+  if (completed) {
+    const { data: requestsUpdated, error: errorThree } = await supabase
+      .from('ride_now_requests')
+      .update({
+        status: 'completed',
+      })
+      .match({ group_id: groupId })
+
+    if (errorThree) {
+      console.error('Error updating requests:', errorThree);
+    } else {
+      console.log('Requests updated');
+    }
   }
 
   res.json({ ok: true, action: 'accept' });
