f1b4b122895e6ed2a7696d8579b1bb4a9297e2ee
Jesslyn Gunadi <jesslyngunadi33@gmail.com>
2025-08-18 13:42:34 -0400

    accept and leave endpoints make db update on ride_request status field

--- server/handlers.js
+++ server/handlers.js
@@ -1,11 +1,41 @@
 import { supabase } from '../client/supabase.js';
 
 export async function accept(req, res) {
-  console.log('accept');
+  console.log('accept endpoint hit');
+  const { id } = req.params;
+
+  const { data, error } = await supabase
+    .from('ride_now_requests')
+    .update({
+      status: 'accepted',
+    })
+    .match({ id: id });
+
+  if (error) {
+    console.error('Error updating row:', error);
+  } else {
+    console.log('Row updated');
+  }
+
   res.json({ ok: true, action: 'accept' });
 }
 
 export async function leave(req, res) {
-  console.log('leave');
+  console.log('leave endpoint hit');
+  const { id } = req.params;
+
+  const { data, error } = await supabase
+    .from('ride_now_requests')
+    .update({
+      status: 'left',
+    })
+    .match({ id: id });
+
+  if (error) {
+    console.error('Error updating row:', error);
+  } else {
+    console.log('Row updated');
+  }
+
   res.json({ ok: true, action: 'leave' });
 }
